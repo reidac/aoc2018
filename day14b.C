4aba2217262ed416d555702adf2597c5b2d64c4a
Andrew Reid <pi@pi>
2018-12-14 19:57:45 -0500

    More ints, fewer strings, 25% speed up, still v. slow on RPi.

--- day14b.C
+++ day14b.C
@@ -3,39 +3,50 @@
 #include <string>
 #include <vector>
 
-#define INPUT 824501
+
+#define INPUT "824501"
+// #define INPUT "51589"
 
 int main() {
-  std::vector<int> recipes = {3,7}; // Also input.
+  std::vector<char> recipes;
+  recipes.reserve(21000000);
+  recipes.push_back('3');
+  recipes.push_back('7');
   int el0 = 0;
   int el1 = 1;
-  
-  std::stringstream is;
-  is << INPUT;
-  std::string istring = is.str();
+
+  std::string istring = INPUT;
   int isize = istring.size(); // # of digits in the input, as a string.
   
   bool done = false;
   int sofar = 0;
   while (!done) {
-    int score = recipes[el0]+recipes[el1];
+
+    int el0i = recipes[el0]-'0';
+    int el1i = recipes[el1]-'0';
+    
+    int score = el0i+el1i;
+    
     std::stringstream ss;
     ss << score;
     std::string scorestr = ss.str();
-    for(int c=0; c<scorestr.size(); ++c) {
-      recipes.push_back(std::stoi(scorestr.substr(c,1)));
+    for(auto &sdx : scorestr) {
+      recipes.push_back(sdx);
     }
-    el0 = (el0+1+recipes[el0])%(recipes.size());
-    el1 = (el1+1+recipes[el1])%(recipes.size());
+
+    el0 = (el0+1+(recipes[el0]-'0'))%(recipes.size());
+    el1 = (el1+1+(recipes[el1]-'0'))%(recipes.size());
 
     while ((sofar+isize)<recipes.size()) {
-      bool matched = true;
+      done = true;
       for(int idx=0;idx<isize;++idx) {
-	if (std::stoi(istring.substr(idx,1))!=recipes[sofar+idx]) matched=false;
+	if (istring[idx]!=recipes[sofar+idx]) {
+	  done = false;
+	  break;
+	}
       }
-      if (matched) {
-	done=true;
-	break;
+      if (done) {
+	break; // Breaks out of the "sofar<size" loop.
       }
       else {
 	sofar += 1;
